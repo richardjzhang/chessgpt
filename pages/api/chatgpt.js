1c5147af3d4434fdb5398e931e24babe0044fc28
richardjzhang <richardjzhang@outlook.com>
2023-03-02 23:48:32 +1100

    Use ChatGPT official api

--- pages/api/chatgpt.js
+++ pages/api/chatgpt.js
@@ -1,4 +1,4 @@
-import { ChatGPTUnofficialProxyAPI } from "chatgpt";
+import { ChatGPTAPI } from "chatgpt";
 import {
   initialChessPrompt,
   invalidMovePrompt,
@@ -6,46 +6,46 @@ import {
 } from "@/constants/prompts";
 
 let parentMessageId;
-let conversationId;
+const MAX_RETRIES = 10;
+const api = new ChatGPTAPI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 function parseMove(move) {
   return move.split("Move: ")[1];
 }
 
 export default async function handler(req, res) {
-  const api = new ChatGPTUnofficialProxyAPI({
-    accessToken: process.env.OPENAI_ACCESS_TOKEN,
-  });
   const { move, color, possibleMoves } = req.query;
   const isFirstMove = JSON.parse(req.query.isFirstMove || false);
 
   if (isFirstMove) {
     parentMessageId = undefined;
-    conversationId = undefined;
   }
 
-  let response = await api.sendMessage(
-    isFirstMove
-      ? initialChessPrompt({ color, move })
-      : nextMovePrompt({ move, possibleMoves }),
-    { conversationId, parentMessageId }
-  );
+  const prompt = isFirstMove
+    ? initialChessPrompt({ color, move })
+    : nextMovePrompt({ move, possibleMoves });
+  let response = await api.sendMessage(prompt, { parentMessageId });
   parentMessageId = response.id;
-  if (!conversationId) conversationId = response.conversationId;
+
   let nextMove = parseMove(response.text);
 
+  let i = 0;
   while (
     possibleMoves &&
     !possibleMoves
       .split(",")
       .map((m) => m.trim())
-      .includes(nextMove)
+      .includes(nextMove) &&
+    i < MAX_RETRIES
   ) {
     response = await api.sendMessage(invalidMovePrompt({ possibleMoves }), {
-      conversationId,
       parentMessageId,
     });
+    parentMessageId = response.id;
     nextMove = parseMove(response.text);
+    i++;
   }
 
   res.status(200).json(nextMove);
