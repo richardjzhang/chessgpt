daeb187790cdb411295afbe61da792c7ebdeb95d
richardjzhang <richardjzhang@outlook.com>
2023-03-23 10:50:27 +1100

    Add warning banner

--- components/ChessGame.jsx
+++ components/ChessGame.jsx
@@ -22,13 +22,18 @@ const ChessGame = ({ playerColor }) => {
 
   return (
     <div className="w-full flex flex-col items-center">
-      <div className="mb-8">
+      <div className="mb-4">
         <Logo className={`relative z-10 ${isFetching ? "animate-spin" : ""}`} />
         <div className="flex justify-center ml-16 -mt-7 relative">
           <div className="px-2 py-0.5 rounded w-fit bg-emerald-500 text-gray-900 text-xs font-semibold">
             Beta
           </div>
         </div>
+      </div>
+      <div className="mb-6 p-4 rounded-lg w-full flex items-center text-center font-semibold bg-gray-800 text-yellow-200">
+        OpenAI is currently experiencing a heavy amount of traffic, so the game
+        may pause unexpectedly as we may not receive a response 😅. Apologies
+        for the inconvenience!
       </div>
       <div className="w-full flex flex-col space-y-10 lg:flex lg:h-[31rem] lg:space-x-20 lg:flex-row lg:space-y-0">
         <div className="w-full max-h-full lg:w-full">
